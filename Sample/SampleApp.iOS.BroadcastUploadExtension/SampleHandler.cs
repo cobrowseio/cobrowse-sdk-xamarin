d71f78ca2a1d36c5c329f5db08cbe43a5bdc5cfb
Mikalai Daronin <me@mikalaidaronin.info>
2020-02-09 11:29:39 +0300

    Finish bindings for CobrowseIOAppExtension.framework.

---
+++ Sample/SampleApp.iOS.BroadcastUploadExtension/SampleHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CobrowseIOSdk.AppExtension;
+using Foundation;
+
+namespace SampleApp.iOS.BroadcastUploadExtension
+{
+    [Register("SampleHandler")]
+    public class SampleHandler : CobrowseIOReplayKitExtension
+    {
+        protected SampleHandler(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+    }
+}
