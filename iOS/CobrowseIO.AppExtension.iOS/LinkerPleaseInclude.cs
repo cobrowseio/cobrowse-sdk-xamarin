ef6fd58cbf844ebe79473ec343aafc1e208043fa
Mikalai Daronin <me@mikalaidaronin.info>
2020-02-14 15:45:49 +0300

    Add explicit reference from 'CobrowseIOAppExtension.framework' to 'Starscream' and 'SwiftCBOR'.

---
+++ iOS/CobrowseIO.AppExtension.iOS/LinkerPleaseInclude.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace CobrowseIOSdk.AppExtension
+{
+    [Foundation.Preserve(AllMembers = true)]
+    public class LinkerPleaseInclude
+    {
+        public LinkerPleaseInclude()
+        {
+        }
+
+        public static void Preserve()
+        {
+
+        }
+
+        public void Include(CobrowseIOReplayKitExtension _)
+        {
+            _ = new CobrowseIOReplayKitExtension();
+        }
+
+        public void Include(Starscream.iOS.LinkerPleaseInclude _)
+        {
+            _ = new Starscream.iOS.LinkerPleaseInclude();
+        }
+
+        public void Include(SwiftCBOR.iOS.LinkerPleaseInclude _)
+        {
+            _ = new SwiftCBOR.iOS.LinkerPleaseInclude();
+        }
+    }
+}
