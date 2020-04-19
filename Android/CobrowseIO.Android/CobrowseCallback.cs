4e697cea285ca0b4080686625e0b51619b01996a
Mikalai Daronin <me@mikalaidaronin.info>
2020-04-19 10:18:18 +0300

    Implement custom 6-digits code activity.

---
+++ Android/CobrowseIO.Android/CobrowseCallback.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Android.Runtime;
+using Java.Lang;
+
+namespace Xamarin.CobrowseIO
+{
+    public delegate void CobrowseCallbackDelegate(Error e, Session s);
+
+    public class CobrowseCallback : Java.Lang.Object, Xamarin.CobrowseIO.ICallback
+    {
+        private readonly CobrowseCallbackDelegate _delegate;
+
+        public CobrowseCallback(CobrowseCallbackDelegate @delegate)
+        {
+            _delegate = @delegate ?? throw new ArgumentNullException(nameof(@delegate));
+        }
+
+        public void Call(Java.Lang.Object err, Java.Lang.Object session)
+        {
+            _delegate.Invoke(err.JavaCast<Error>(), session.JavaCast<Session>());
+        }
+
+        public void Call(Java.Lang.Error err, Session session)
+        {
+            _delegate.Invoke(err, session);
+        }
+    }
+}
