7fbe2a5241cde366b2d67c610106eb87d1740024
Mikalai Daronin <me@mikalaidaronin.info>
2021-07-13 22:31:51 +0300

    Fix Java-C# enum mapping.

--- Android/CobrowseIO.Android/Additions/Session.cs
+++ Android/CobrowseIO.Android/Additions/Session.cs
@@ -13,5 +13,17 @@ namespace Xamarin.CobrowseIO
         {
             this.End(new CobrowseCallback<Java.Lang.Error, Session>(@delegate));
         }
+
+        public bool FullDevice => this._FullDevice().BooleanValue();
+
+        public void SetFullDevice(bool value, CobrowseCallbackDelegate<Java.Lang.Error, Session> @delegate)
+        {
+            this.SetFullDevice(value, new CobrowseCallback<Java.Lang.Error, Session>(@delegate));
+        }
+
+        public void SetRemoteControl(Xamarin.CobrowseIO.RemoteControlState state, CobrowseCallbackDelegate<Java.Lang.Error, Session> @delegate)
+        {
+            this.SetRemoteControl(state, new CobrowseCallback<Java.Lang.Error, Session>(@delegate));
+        }
     }
 }
