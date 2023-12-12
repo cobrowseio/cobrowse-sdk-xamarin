c9040277b5e9dd8bf2e3d47e454ca9830cc52262
Mikalai Daronin <me@mikalaidaronin.info>
2023-12-13 00:01:00 +0300

    fix: make sure the iOS full-device mode is working

--- Sample/MauiSample.iOS.BroadcastUploadExtension/SampleHandler.cs
+++ Sample/MauiSample.iOS.BroadcastUploadExtension/SampleHandler.cs
@@ -5,10 +5,12 @@ using Xamarin.CobrowseIO.iOS.AppExtension;
 
 namespace MauiSample.iOS.BroadcastUploadExtension
 {
+    [Register("SampleHandler")]
     public class SampleHandler : CobrowseIOReplayKitExtension
     {
         protected SampleHandler (IntPtr handle) : base (handle)
         {
+            // Note: this .ctor should not contain any initialization logic.
         }
     }
 }
